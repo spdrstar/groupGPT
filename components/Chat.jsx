6606a033411a899d0753e84f236eca536517482d
Zach Eisenhauer <spdrstar@gmail.com>
2023-03-26 01:00:03 -0700

    proper commit

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,6 +1,6 @@
 import SendbirdApp from '@sendbird/uikit-react/App';
-const APP_ID = "9C0ADFA0-D770-4092-B9DD-340B4721CB7C";
-const USER_ID = "sendbirdian-200720";
+const APP_ID = process.env.APP_ID;
+const USER_ID = process.env.USER_ID;
 
 const Chat = () => (
     <div style={{ height: "100vh", width: "100vw" }}>
